d72bce18cf7290dbac599e369629995d51e29579
Andrei2381283 <kirill.andrei11@gmail.com>
2023-05-26 17:58:36 +0300

    Burger menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+import { Offcanvas } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default function Header() {
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
 	return <header className="d-flex bg-dark px-5 py-3">
-		<nav>
-			<ul className="d-flex list-unstyled m-0">
-				<li><Link className="text-white text-decoration-none" to="/">Main</Link></li>
-				<li><Link className="text-white text-decoration-none ms-4" to="/about-me">About me</Link></li>
-			</ul>
-		</nav>
+		<button className="bg-transparent" style={{ width: "44px" }} onClick={handleShow}>
+			<div className="border w-100 my-2"></div>
+			<div className="border w-100"></div>
+			<div className="border w-100 my-2"></div>
+		</button>
+		<Offcanvas show={show} onHide={handleClose}>
+			<Offcanvas.Header closeButton>
+				<Offcanvas.Title>Menu</Offcanvas.Title>
+			</Offcanvas.Header>
+			<Offcanvas.Body>
+				<nav>
+					<ul className="list-unstyled m-0">
+						<li><Link className="text-black fs-5" to="/">Main</Link></li>
+						<li className="mt-2"><Link className="text-black fs-5" to="/about-me">About me</Link></li>
+					</ul>
+				</nav>
+			</Offcanvas.Body>
+		</Offcanvas>
 	</header>
 }
